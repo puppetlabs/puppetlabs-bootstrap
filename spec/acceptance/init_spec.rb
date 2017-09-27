6d73ef3cecabcab0d8db8ff32f60b71e502c19dd
Hunter Haugen <hunter@puppet.com>
2017-09-27 15:45:28 -0700

    Cat cert content instead of checking fingerprint

--- spec/acceptance/init_spec.rb
+++ spec/acceptance/init_spec.rb
@@ -5,8 +5,8 @@ describe 'bootstrap task' do
   describe 'install', if: pe_install? do
     it 'installs the agent' do
       fingerprint = on(master, "puppet cert list #{master.hostname}").stdout.match(%r{[0-9A-F:]{95}})[0]
-      result = run_bolt_task(task_name: 'bootstrap', params: "master=#{master.hostname} ca_fingerprint=#{fingerprint}")
-      expect_multiple_regexes(result: result, regexes: [%r{status : installed}, %r{version : 1.\d}, %r{Job completed. 1/1 nodes succeeded}])
+      result = run_bolt_task(task_name: 'bootstrap', params: "master=#{master.hostname} cacert_content=\"$(cat /etc/puppetlabs/puppet/ssl/certs/ca.pem)\"")
+      expect_multiple_regexes(result: result, regexes: [%r{Installed}, %r{version : 1.\d}, %r{Job completed. 1/1 nodes succeeded}])
     end
   end
 end
--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -7,13 +7,15 @@ def install_bolt_on(hosts)
   on(hosts, "/opt/puppetlabs/puppet/bin/gem install --source http://rubygems.delivery.puppetlabs.net bolt -v '> 0.0.1'", acceptable_exit_codes: [0, 1]).stdout
 end
 
+def pe_install?
+  ENV['PUPPET_INSTALL_TYPE'] =~ %r{pe}i
+end
+
 run_puppet_install_helper_on(master)
-install_ca_certs_on(master) unless ENV['PUPPET_INSTALL_TYPE'] =~ %r{pe}i
+install_ca_certs_on(master) unless pe_install?
 install_bolt_on(master)
 install_module_on(master)
 install_module_dependencies_on([master])
-
-UNSUPPORTED_PLATFORMS = %w[Windows Solaris AIX].freeze
 
 DEFAULT_PASSWORD = if master[:hypervisor] == 'vagrant'
                      'vagrant'
@@ -25,10 +27,6 @@ def run_puppet_access_login(user:, password: '~!@#$%^*-/ aZ', lifetime: '5y')
   on(master, puppet('access', 'login', '--username', user, '--lifetime', lifetime), stdin: password)
 end
 
-def pe_install?
-  ENV['PUPPET_INSTALL_TYPE'] =~ %r{pe}i
-end
-
 def run_task(task_name:, params: nil, password: DEFAULT_PASSWORD)
   if pe_install?
     run_puppet_task(task_name: task_name, params: params)
@@ -38,11 +36,11 @@ def run_task(task_name:, params: nil, password: DEFAULT_PASSWORD)
 end
 
 def run_bolt_task(task_name:, params: nil, password: DEFAULT_PASSWORD)
-  on(master, "/opt/puppetlabs/puppet/bin/bolt task run #{task_name} --modules /etc/puppetlabs/code/modules/service --nodes localhost --password #{password} #{params}", acceptable_exit_codes: [0, 1]).stdout # rubocop:disable Metrics/LineLength
+  on(master, "/opt/puppetlabs/puppet/bin/bolt task run #{task_name} --modules /etc/puppetlabs/code/modules --nodes #{default.hostname} --password #{password} #{params}", acceptable_exit_codes: [0, 1]).stdout # rubocop:disable Metrics/LineLength
 end
 
 def run_puppet_task(task_name:, params: nil)
-  on(master, puppet('task', 'run', task_name, '--nodes', fact_on(master, 'fqdn'), params.to_s), acceptable_exit_codes: [0, 1]).stdout
+  on(master, puppet('task', 'run', task_name, '--nodes', fact_on(default, 'fqdn'), params.to_s), acceptable_exit_codes: [0, 1]).stdout
 end
 
 def expect_multiple_regexes(result:, regexes:)
