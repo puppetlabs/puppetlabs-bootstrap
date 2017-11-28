b24beed0ed9a5e03750600051c40c00acd7cc01e
Will Meek <william.meek@puppet.com>
2017-11-28 17:29:18 +0000

    Test Cleanup

--- spec/acceptance/init_spec.rb
+++ spec/acceptance/init_spec.rb
@@ -1,12 +1,21 @@
 # run a test task
 require 'spec_helper_acceptance'
 
+def get_hostname
+  host = if pe_install?
+    master.hostname
+  else
+    'localhost'
+         end
+  host
+end
+
 describe 'bootstrap task' do
-  describe 'install', if: pe_install? do
+  describe 'install' do
     it 'installs the agent' do
-      fingerprint = on(master, "puppet cert list #{master.hostname}").stdout.match(%r{[0-9A-F:]{95}})[0]
-      result = run_bolt_task(task_name: 'bootstrap', params: "master=#{master.hostname} cacert_content=\"$(cat /etc/puppetlabs/puppet/ssl/certs/ca.pem)\"")
-      expect_multiple_regexes(result: result, regexes: [%r{Installed}, %r{Ran on 1 node}])
+      on(master, "puppet cert list #{get_hostname}").stdout.match(%r{[0-9A-F:]{95}})[0] if pe_install?
+      result = run_task(task_name: 'bootstrap', params: "master=#{get_hostname} cacert_content=\"$(cat /etc/puppetlabs/puppet/ssl/certs/ca.pem)\"")
+      expect_multiple_regexes(result: result, regexes: [%r{Installed}, %r{Job completed. 1/1 nodes succeeded|Ran on 1 node}])
     end
   end
 end
--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -2,52 +2,13 @@ require 'puppet'
 require 'beaker-rspec'
 require 'beaker/puppet_install_helper'
 require 'beaker/module_install_helper'
+require 'beaker/task_helper'
 
-def install_bolt_on(hosts)
-  on(hosts, "/opt/puppetlabs/puppet/bin/gem install --source http://rubygems.delivery.puppetlabs.net bolt -v '> 0.0.1'", acceptable_exit_codes: [0, 1]).stdout
-end
-
-def pe_install?
-  ENV['PUPPET_INSTALL_TYPE'] =~ %r{pe}i
-end
-
-run_puppet_install_helper_on(master)
-install_ca_certs_on(master) unless pe_install?
-install_bolt_on(master)
-install_module_on(master)
-install_module_dependencies_on([master])
-
-DEFAULT_PASSWORD = if master[:hypervisor] == 'vagrant'
-                     'vagrant'
-                   elsif master[:hypervisor] == 'vcloud'
-                     'Qu@lity!'
-                   end
-
-def run_puppet_access_login(user:, password: '~!@#$%^*-/ aZ', lifetime: '5y')
-  on(master, puppet('access', 'login', '--username', user, '--lifetime', lifetime), stdin: password)
-end
-
-def run_task(task_name:, params: nil, password: DEFAULT_PASSWORD)
-  if pe_install?
-    run_puppet_task(task_name: task_name, params: params)
-  else
-    run_bolt_task(task_name: task_name, params: params, password: password)
-  end
-end
-
-def run_bolt_task(task_name:, params: nil, password: DEFAULT_PASSWORD)
-  on(master, "/opt/puppetlabs/puppet/bin/bolt task run #{task_name} --modules /etc/puppetlabs/code/modules --nodes #{default.hostname} --password #{password} #{params}", acceptable_exit_codes: [0, 1]).stdout # rubocop:disable Metrics/LineLength
-end
-
-def run_puppet_task(task_name:, params: nil)
-  on(master, puppet('task', 'run', task_name, '--nodes', fact_on(default, 'fqdn'), params.to_s), acceptable_exit_codes: [0, 1]).stdout
-end
-
-def expect_multiple_regexes(result:, regexes:)
-  regexes.each do |regex|
-    expect(result).to match(regex)
-  end
-end
+run_puppet_install_helper
+install_ca_certs unless pe_install?
+install_bolt_on(hosts) unless pe_install?
+install_module_on(hosts)
+install_module_dependencies_on(hosts)
 
 RSpec.configure do |c|
   # Readable test descriptions
@@ -55,6 +16,6 @@ RSpec.configure do |c|
 
   # Configure all nodes in nodeset
   c.before :suite do
-    run_puppet_access_login(user: 'admin')
+    run_puppet_access_login(user: 'admin') if pe_install?
   end
 end
