a73958a638db703fc2082012ff6631b1f0325045
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2018-01-12 10:23:31 +0100

    Update and rename init_spec.rb to linux_spec.rb

--- spec/acceptance/linux_spec.rb
+++ spec/acceptance/linux_spec.rb
@@ -14,7 +14,7 @@ describe 'bootstrap task' do
   describe 'install' do
     it 'installs the agent' do
       on(master, "puppet cert list #{get_hostname}").stdout.match(%r{[0-9A-F:]{95}})[0] if pe_install?
-      result = run_task(task_name: 'bootstrap', params: "master=#{get_hostname} cacert_content=\"$(cat /etc/puppetlabs/puppet/ssl/certs/ca.pem)\"")
+      result = run_task(task_name: 'bootstrap::linux', params: "master=#{get_hostname} cacert_content=\"$(cat /etc/puppetlabs/puppet/ssl/certs/ca.pem)\"")
       expect_multiple_regexes(result: result, regexes: [%r{Installed}, %r{Job completed. 1/1 nodes succeeded|Ran on 1 node}])
     end
   end
