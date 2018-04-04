a5d1c85354b9ce0d4a78c42b96be00d82505e27e
Iristyle <Iristyle@github>
2018-04-03 17:14:23 -0700

    (MODULES-6831) Use Root CA to verify install.ps1
     - Previously SSL was completely disabled when downloading the
       install.ps1
    
       Instead, provide a fully implemented validation callback that
       examines the policy errors and certificate chain, and fails to
       download the install.ps1 unless:
    
       * there are no policy errors (meaning the root cetificate authority
         that signed the masters cert is already trusted on the machine,
         which could indicate existing CA infra in use)
       * the error is the result of a self-signed cert for the root
         certificate authority *and* that cert matches the one passed
         in to the helper
    
     - Provide some additional information around the download of the
       install.ps1 script
    
     - Add a new helper function that converts a cert string into an
       X509Certificate2 object

--- tasks/windows.ps1
+++ tasks/windows.ps1
@@ -54,6 +54,73 @@ function Get-CA($Master)
   }
 }
 
+function New-CertificateFromContent($Content)
+{
+  $bytes = [System.Text.Encoding]::UTF8.GetBytes($Content)
+  $cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2 -ArgumentList @(,$bytes)
+  $cert.Import($bytes)
+
+  return $cert
+}
+
+function Get-InstallerScriptBlock($Master, $RootCertificate)
+{
+  $verificationCallback = [Net.ServicePointManager]::ServerCertificateValidationCallback
+  try
+  {
+    $customCACallback = {
+      param(
+        $sender,
+        [System.Security.Cryptography.X509Certificates.X509Certificate]
+        $certificate,
+        [System.Security.Cryptography.X509Certificates.X509Chain]
+        $chain,
+        [System.Net.Security.SslPolicyErrors]
+        $sslPolicyErrors
+      )
+
+      # when there are no policy errors the root certificate used to sign the
+      # masters certificate is either:
+      # * in the local trusted cert store already (other CA infra / intermediate
+      #   CA already in place)
+      # * is not self-signed / generated from a valid third-party root CA
+      if ($sslPolicyErrors -eq 'None') { return $true }
+
+      # Build a new chain that includes the Root Certificate Authority
+      $chain.ChainPolicy.ExtraStore.Add($RootCertificate)
+      $chain.Build($certificate)
+
+      # For a Puppet server response to typically be valid:
+
+      # 1. there should only be a single status
+      $oneStatus = $chain.ChainStatus.Count -eq 1
+      # 2. the chain terminated with a status of UntrustedRoot (self-signed) cert
+      # not one of the 20+ other possible failure modes
+      # https://msdn.microsoft.com/en-us/library/windows/apps/xaml/system.security.cryptography.x509certificates.x509chainstatusflags(v=vs.90).aspx
+      $untrustedRoot = [System.Security.Cryptography.X509Certificates.X509ChainStatusFlags]::UntrustedRoot
+      $untrustedCA = $chain.ChainStatus[0].Status -eq $untrustedRoot
+      # 3. the end of the chain (root) is the same CA as passed in
+      $lastCertInChain = $chain.ChainElements[$chain.ChainElements.Count - 1].Certificate
+      $rootIsExpectedCA = $lastCertInChain -eq $RootCertificate
+
+      return $oneStatus -and $untrustedCA -and $rootIsExpectedCA
+    }
+
+    # Allow install.ps1 from an endpoint signed by the Root Certificate Authority
+    [Net.ServicePointManager]::ServerCertificateValidationCallback = $customCACallback
+    $installerUri = "https://${Master}:8140/packages/current/install.ps1"
+    Write-Verbose "Downloading simplified installer $installerUri - allowing root cert [$($RootCertificate.Thumbprint)]"
+    $installer = (New-Object System.Net.WebClient).DownloadString($installerUri)
+
+    return [ScriptBlock]::Create($installer)
+  }
+  finally
+  {
+    # restore original chain validation
+    [Net.ServicePointManager]::ServerCertificateValidationCallback = $verificationCallback
+  }
+}
+
 if (!$PSBoundParameters.ContainsKey('CertName'))
 {
   $CertName = Get-HostName
@@ -67,10 +134,9 @@ if (!$PSBoundParameters.ContainsKey('CACert_Content') -or [String]::IsNullOrEmpt
 New-Item -ItemType Directory -Force -Path "$env:ProgramData\PuppetLabs\puppet\etc\ssl\certs"
 Out-File -InputObject $CACert_Content -FilePath "$env:ProgramData\PuppetLabs\puppet\etc\ssl\certs\ca.pem" -Encoding ascii -Force
 
-[Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
-$installerUri = "https://${Master}:8140/packages/current/install.ps1"
-$installer = (New-Object System.Net.WebClient).DownloadString($installerUri)
-$installer = [ScriptBlock]::Create($installer)
+$MasterCA = New-CertificateFromContent -Content $CACert_Content
+$installer = Get-InstallerScriptBlock -Master $Master -RootCertificate $MasterCA
+$MasterCA.Dispose()
 
 $installerArgs = @{
   Arguments = @("agent:certname='$CertName'")
