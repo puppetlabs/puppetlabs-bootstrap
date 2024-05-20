30f6c7e2e6a3d92f882cd6b4c5f2714ad0a7b5a5
ben <benrobertson9876@gmail.com>
2024-05-20 11:38:26 +1000

    Set test on PR only

--- spec/acceptance/linux_spec.rb
+++ spec/acceptance/linux_spec.rb
@@ -13,31 +13,11 @@ def return_hostname
 end
 
 describe 'bootstrap task' do
-  include BoltSpec::Run
-  let(:bolt_config) { { 'modulepath' => RSpec.configuration.module_path } }
-  let(:bootstrap_dir) { File.join(RSpec.configuration.module_path, '../docker_provision') }
-  # let(:expected_default_output) { { 'clivar' => 'override', 'tfstatevar' => 'foo' } }
-  # let(:expected_modified_output) { { 'clivar' => 'foo', 'tfstatevar' => 'bar' } }
-  # let(:var_file) { 'tfvar_alternate.tfvars' }
-  # describe 'install' do
-  #   it 'installs the agent',  pending: 'Test needs re-written and nodesets fixed' do
-  #     on(master, "puppet cert list #{return_hostname}").stdout.match(%r{[0-9A-F:]{95}})[0] if pe_install?
-  #     result = run_task(task_name: 'bootstrap::linux', params: "master=#{return_hostname} cacert_content=\"$(cat /etc/puppetlabs/puppet/ssl/certs/ca.pem)\"")
-  #     expect_multiple_regexes(result: result, regexes: [%r{Installed}, %r{Job completed. 1/1 nodes succeeded|Ran on 1 node}])
-  #   end
-  # end
-
-  before(:all) do
-    bolt_config = { 'modulepath' => RSpec.configuration.module_path }
-    bootstrap_dir = File.join(RSpec.configuration.module_path, '../docker_provision')
-    puts bolt_config
-    puts bootstrap_dir
-    result = run_plan('deploy_pe::provision_master', 'targets' => 'localhost', 'version' => '2021.7.8')
-    expect(result['status']).to eq('success')
-  end
-
-  it 'installs Puppet agent' do
-    result = run_task('bootstrap', 'localhost', { 'master' => 'localhost', 'certname' => 'testing123.com' })
-    expect(result['status']).to eq('success')
+  describe 'install' do
+    it 'installs the agent',  pending: 'Test needs re-written and nodesets fixed' do
+      on(master, "puppet cert list #{return_hostname}").stdout.match(%r{[0-9A-F:]{95}})[0] if pe_install?
+      result = run_task(task_name: 'bootstrap::linux', params: "master=#{return_hostname} cacert_content=\"$(cat /etc/puppetlabs/puppet/ssl/certs/ca.pem)\"")
+      expect_multiple_regexes(result: result, regexes: [%r{Installed}, %r{Job completed. 1/1 nodes succeeded|Ran on 1 node}])
+    end
   end
 end
